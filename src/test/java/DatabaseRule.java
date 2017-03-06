0058cc0b72893792535e02cc67893238dbada665
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-03-06 17:13:10 +0300

    finished client and designer files

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -3,17 +3,18 @@ import org.sql2o.*;
 public class DatabaseRule extends ExternalResource{
     @Override
     protected  void before(){
-      DB.sql2o=new Sql2o("jdbc:postgresql://localhost:5432/name-of-database","james","admin");
+      DB.sql2o=new Sql2o("jdbc:postgresql://localhost:5432/kitenge_test","james","admin");
 
     }
 
     @Override
     protected  void after(){
       try(Connection con =DB.sql2o.open()){
+        String sqlCart="delete from cart *";
+        String sqlDesigner="delete from designer *";
 
-
+        con.createQuery(sqlCart).executeUpdate();
+        con.createQuery(sqlDesigner).executeUpdate();
 }
     }
-
-
-  }
+}
